f0af5d1b62a7ee4acd9f2104514cb127fe8f5e6b
Brian K <info@0daysoundz.nl>
2013-08-01 01:41:30 +0200

    random updates for local usage

--- application/config/config.php
+++ application/config/config.php
@@ -272,7 +272,7 @@ $config['cache_path'] = '';
 | http://codeigniter.com/user_guide/libraries/sessions.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'UYGUYGUYG3246fdhgdyg3783';
 
 /*
 |--------------------------------------------------------------------------
